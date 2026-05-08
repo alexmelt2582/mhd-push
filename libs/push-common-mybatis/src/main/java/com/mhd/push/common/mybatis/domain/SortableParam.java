b42b5e47e875380c0c0102849909c99eb6966f3b
zhd-xiaoxiao16 <zhaohaodong29@163.com>
2026-05-08 22:49:51 +0800

    update：libs模块添加mybatis公共模块

---
+++ libs/push-common-mybatis/src/main/java/com/mhd/push/common/mybatis/domain/SortableParam.java
@@ -0,0 +1,39 @@
+package com.mhd.push.common.mybatis.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+/**
+ * 排序查询参数
+ *
+ * @author zhao-hao-dong
+ **/
+@Setter
+@Getter
+@ToString
+public class SortableParam {
+    private List<String> sortingFields;
+
+    public static class SortingField {
+        /**
+         * 顺序 - 升序
+         */
+        public static final String ORDER_ASC = "asc";
+        /**
+         * 顺序 - 降序
+         */
+        public static final String ORDER_DESC = "desc";
+        /**
+         * 字段
+         */
+        private String field;
+        /**
+         * 顺序
+         */
+        private String order;
+
+    }
+}
